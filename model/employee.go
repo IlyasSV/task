7c127176e7f3a0ed492621e32f9108b56e7dcaec
Ilyas <bdkydrov@gmail.com>
2024-08-07 16:01:35 +0300

    1. В задачи сказано Инициализируйте несколько экземпляров. Таким образом их нужно собрать в один слайс и пройдясь по каждому элементу в слайсе вызвать функцию PrintEmployee 2. Можно было здесь использовать (e *Employee) где e была бы указателем на Employee

--- model/employee.go
+++ model/employee.go
@@ -9,6 +9,6 @@ type Employee struct {
 	Position string
 }
 
-func (e Employee) PrintEmployee() {
+func (e *Employee) PrintEmployee() {
 	fmt.Printf("Информация о сотруднике:\nИмя: %s Возвраст: %d\nАдрес: %+v\nPosition: %s\n", e.Name, e.Age, e.Address, e.Position)
 }
--- task1/main.go
+++ task1/main.go
@@ -1,67 +1,68 @@
 package main
 
 import (
+	"fmt"
 	"taskValihan/model"
 )
 
 func main() {
 
-	//Задача 2: Инициализация структуры
-	//Инициализируйте несколько экземпляров структуры Person и выведите их на экран.
-
-	aaa := model.Person{
-		Name: "AAA",
-		Age:  20,
-		Address: model.Address{
-			City:       "Иннополис",
-			Street:     "ул. Спортивная, 132",
-			PostalCode: 123456,
-		},
-	}
-
-	bbb := model.Person{Name: "BBB", Age: 21, Address: model.Address{City: "Казань",
-		Street:     "ул. Спортивная, 132",
-		PostalCode: 123456}}
-
-	aaa.Print()
-	bbb.Print()
-
-	//Задача 3: Изменение полей структуры
-	//Измените значение поля Address у одного из экземпляров структуры Person
-	//и выведите обновленный экземпляр на экран.
-
-	bbb.Name = "ABC"
-	bbb.Print()
-
-	aaa.Age = 30
-	aaa.Print()
-
-	//Задача 4: Методы для структур
-	//Добавьте метод Greet для структуры Person, который будет выводить приветствие
-	//с именем человека.
-
-	model.Greet(bbb)
-
-	//Задача 6: Инициализация вложенной структуры
-	//Инициализируйте структуру Company и выведите информацию о компании и её сотрудниках.
-
-	emp1 := model.Person{
-		Name: "RTY",
-		Age:  20,
-		Address: model.Address{
-			City:       "Москва",
-			Street:     "ул. Спортивная, 132",
-			PostalCode: 123456},
-	}
-
-	emp1.Print()
-
-	company := model.Company{
-		Name:      "RED",
-		Employees: []model.Person{emp1},
-	}
-
-	company.PrintCompany()
+	////Задача 2: Инициализация структуры
+	////Инициализируйте несколько экземпляров структуры Person и выведите их на экран.
+	//
+	//aaa := model.Person{
+	//	Name: "AAA",
+	//	Age:  20,
+	//	Address: model.Address{
+	//		City:       "Иннополис",
+	//		Street:     "ул. Спортивная, 132",
+	//		PostalCode: 123456,
+	//	},
+	//}
+	//
+	//bbb := model.Person{Name: "BBB", Age: 21, Address: model.Address{City: "Казань",
+	//	Street:     "ул. Спортивная, 132",
+	//	PostalCode: 123456}}
+	//
+	//aaa.Print()
+	//bbb.Print()
+	//
+	////Задача 3: Изменение полей структуры
+	////Измените значение поля Address у одного из экземпляров структуры Person
+	////и выведите обновленный экземпляр на экран.
+	//
+	//bbb.Name = "ABC"
+	//bbb.Print()
+	//
+	//aaa.Age = 30
+	//aaa.Print()
+	//
+	////Задача 4: Методы для структур
+	////Добавьте метод Greet для структуры Person, который будет выводить приветствие
+	////с именем человека.
+	//
+	//model.Greet(bbb)
+	//
+	////Задача 6: Инициализация вложенной структуры
+	////Инициализируйте структуру Company и выведите информацию о компании и её сотрудниках.
+	//
+	//emp1 := model.Person{
+	//	Name: "RTY",
+	//	Age:  20,
+	//	Address: model.Address{
+	//		City:       "Москва",
+	//		Street:     "ул. Спортивная, 132",
+	//		PostalCode: 123456},
+	//}
+	//
+	//emp1.Print()
+	//
+	//company := model.Company{
+	//	Name:      "RED",
+	//	Employees: []model.Person{emp1},
+	//}
+	//
+	//company.PrintCompany()
 
 	//Задача 10: Использование анонимных полей
 	//Инициализируйте несколько экземпляров структуры Employee и выведите их на экран.
@@ -79,4 +80,87 @@ func main() {
 		Position: "1",
 	}
 	employee1.PrintEmployee()
+
+	employees := []model.Employee{
+		{
+			Person: model.Person{
+				Name: "Deni",
+				Age:  24,
+				Address: model.Address{
+					City:       "Moscow",
+					Street:     "Arbat",
+					PostalCode: 1234,
+				},
+			},
+			Position: "2",
+		},
+		{
+			Person: model.Person{
+				Name: "Nino",
+				Age:  19,
+				Address: model.Address{
+					City:       "Bishkek",
+					Street:     "Micro",
+					PostalCode: 1234,
+				},
+			},
+			Position: "3",
+		},
+		{
+			Person: model.Person{
+				Name: "Roni",
+				Age:  20,
+				Address: model.Address{
+					City:       "Lisobon",
+					Street:     "Pravda",
+					PostalCode: 1234,
+				},
+			},
+			Position: "4",
+		},
+	}
+
+	employees = append(employees, employee1)
+	printEmployees(employees)
+
+	emp2 := []model.Employee{
+		{
+			Person: model.Person{
+				Name: "Titi",
+				Age:  27,
+				Address: model.Address{
+					City:       "Praga",
+					Street:     "Rjaka",
+					PostalCode: 10292,
+				},
+			},
+			Position: "5",
+		},
+		{
+			Person: model.Person{
+				Name: "Yoyo",
+				Age:  24,
+				Address: model.Address{
+					City:       "Riga",
+					Street:     "Kievskaya",
+					PostalCode: 808090,
+				},
+			},
+			Position: "6",
+		},
+	}
+
+	employees = append(employees, emp2...)
+
+	printEmployees(employees)
+
+}
+
+func printEmployees(employees []model.Employee) {
+	for _, emp := range employees {
+		fmt.Println("Информация о сотруднике:")
+		fmt.Printf("Имя: %s, Возврат: %d\nДомашний адрес:\nГород: %s\nУлица: %s\nИндекс:"+
+			" %d\nПозиция: %s\n", emp.Name, emp.Age, emp.Address.City,
+			emp.Address.Street, emp.Address.PostalCode, emp.Position)
+	}
 }
